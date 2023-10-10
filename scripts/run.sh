a85f65918b75bb8b4d32fa4d678c5fafb8154fd0
kevin-xuan <raoxuanzzz@gmail.com>
2023-10-10 12:23:37 +0900

    first commit

--- scripts/run.sh
+++
@@ -1,6 +0,0 @@
-python main.py --gpu 1 --dataset four-sin --num_blocks 3 --num_heads 2 --dropout_rate 0.3 --l2_emb 1e-4 --layer_num 4
-python main.py --gpu 1 --dataset gowalla  --num_blocks 3 --num_heads 2 --dropout_rate 0.2 --l2_emb 1e-3 --layer_num 2 # 不确定?
-
-cd anchor-version
-python main_anchor.py --gpu 0 --dataset four-sin --num_blocks 2 --num_heads 1 --dropout_rate 0.2 --l2_emb 1e-3 --layer_num 3  
-python main_anchor.py --gpu 1 --dataset gowalla --num_blocks 2 --num_heads 1 --dropout_rate 0.2 --l2_emb 1e-3 --layer_num 2  
